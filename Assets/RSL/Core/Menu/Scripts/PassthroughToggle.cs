7048f0470cb91205cad0790b4d1826e8a29329f3
Soldann <guotl321@gmail.com>
2026-05-02 18:29:03 +0200

    High level folder refactor, change Components folder to RSL folder

---
+++ Assets/RSL/Core/Menu/Scripts/PassthroughToggle.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace RSL.Core.Menu
+{
+    public class PassthroughToggle : MonoBehaviour
+    {
+        private ARCameraManager arCameraManager;
+        void Start()
+        {
+            arCameraManager = FindObjectOfType<ARCameraManager>();
+            if (arCameraManager == null)
+            {
+                Debug.LogWarning("ARCameraManager not found in the scene.");
+            }
+        }
+
+        public void SetPassthroughEnabled(int disabled)
+        {
+            if (arCameraManager != null)
+            {
+                arCameraManager.enabled = disabled == 0;
+            }
+        }
+    }
+}
